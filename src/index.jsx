95d1c52450b84ace66441546d5fd895225a5686e
Lokeshwar Dewangan <lokeshwar.prasad.cse@gmail.com>
2026-06-18 16:08:51 +0530

    migrate from create-react-app to vite 8

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import './styles/theme.css';
 import './styles/base.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -21,8 +20,3 @@ root.render(
     </Auth0Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
